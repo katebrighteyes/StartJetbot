a7889087fb6918f6da9f5a1c09895d3bbcf48cd5
Kate <49902595+katebrighteyes@users.noreply.github.com>
2024-05-17 15:48:45 +0900

    Update cleanJetBot.sh

--- cleanJetBot.sh
+++ cleanJetBot.sh
@@ -1,4 +1,7 @@
+rm ./Downloads/*
+rm *.png
 rm -rf ./AILearningJetbot/AEBreak/dataset/
 rm ./AILearningJetbot/AEBreak/best_model.pth 
 rm -rf ./AILearningJetbot/LaneFollowing/dataset_xy/
-
+rm ./AILearningJetbot/LaneFollowing/*.pth
+rm ./AILearningJetbot/LaneFollowing/*.zip
